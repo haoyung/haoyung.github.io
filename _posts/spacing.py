be61e049b26d5947c21caf9c0e3801e141ea0610
TimothyWeng <tim0309.tw@gmail.com>
2023-11-22 17:20:43 +0800

    new post

--- _posts/spacing.py
+++ _posts/spacing.py
@@ -1,26 +1,47 @@
 import os
 import re
 
-# Define a function to add spaces between Chinese and English characters
-def add_spaces_to_md_files():
-    # Get a list of all .md files in the current directory
-    md_files = [f for f in os.listdir() if f.endswith('.md')]
+def add_space_between_chinese_and_english(text):
+    # Regular expression to match Chinese character followed by English/number or vice versa
+    pattern = r'([\u4e00-\u9fff])([A-Za-z0-9])|([A-Za-z0-9])([\u4e00-\u9fff])'
+    def replacement(match):
+        # Groups: Chinese before, English/number after, English/number before, Chinese after
+        chinese_before, english_after, english_before, chinese_after = match.groups()
+        return f'{chinese_before or ""}{english_before or ""} {english_after or ""}{chinese_after or ""}'
 
-    # Define a regular expression pattern to match Chinese characters
-    chinese_pattern = r'[\u4e00-\u9fff]+'
+    # Find all matches and their start positions
+    changes = []
+    for match in re.finditer(pattern, text):
+        start_pos = match.start()
+        original_text = match.group(0)
+        new_text = replacement(match)
+        changes.append((start_pos, original_text, new_text))
 
-    # Iterate through each .md file
-    for md_file in md_files:
-        with open(md_file, 'r', encoding='utf-8') as file:
-            content = file.read()
+    # Apply the replacements
+    modified_text = re.sub(pattern, replacement, text)
+    return modified_text, changes
 
-        # Use regex to find Chinese characters followed by English characters
-        updated_content = re.sub(f'({chinese_pattern})([a-zA-Z]+)', r'\1 \2', content)
+def process_markdown_files(directory):
+    for filename in os.listdir(directory):
+        if filename.endswith('.md'):
+            filepath = os.path.join(directory, filename)
+            with open(filepath, 'r', encoding='utf-8') as file:
+                content = file.read()
 
-        # Write the updated content back to the file
-        with open(md_file, 'w', encoding='utf-8') as file:
-            file.write(updated_content)
+            modified_content, changes = add_space_between_chinese_and_english(content)
 
-if __name__ == "__main__":
-    add_spaces_to_md_files()
-    print("Spaces added between Chinese and English characters in .md files.")
+            with open(filepath, 'w', encoding='utf-8') as file:
+                file.write(modified_content)
+            
+            if changes:
+                print(f"Processed {filename}:")
+                for pos, original, new in changes:
+                    print(f"  At position {pos}: '{original}' -> '{new}'")
+            else:
+                print(f"No changes made to {filename}")
+
+# Get the directory of the script
+script_directory = os.path.dirname(os.path.abspath(__file__))
+
+# Process the Markdown files in the script's directory
+process_markdown_files(script_directory)

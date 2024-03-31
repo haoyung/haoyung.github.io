f39246305a01cfb38e458f69a80161486b39ffe0
TimothyWeng <tim0309.tw@gmail.com>
2024-04-01 04:47:41 +0800

    new post

--- convert_img.py
+++ convert_img.py
@@ -18,41 +18,45 @@ def has_multiple_layers(image_path):
 def convert_image(image_path):
     # Get the image size in bytes
     image_size = get_image_size(image_path)
-
+    
     # Prepare the conversion command
     convert_command = ['convert', image_path]
-
-    if image_size > 2 * 1024 * 1024:  # Check if image size is greater than 2MB
+    
+    if image_size > 2 * 1024 * 1024:
+        # Check if image size is greater than 2MB
         # Add resize flag if the image size is larger than 2MB
         convert_command.extend(['-resize', '50%'])
-
+    
     # Add output file to the command
     convert_command.append(image_path.rsplit('.', 1)[0] + '.webp')
-
+    
     # Execute the conversion command
     subprocess.run(convert_command)
 
 def process_directory(directory):
     # Define the allowed extensions
     extensions = ('.jpg', '.JPG', '.png', '.PNG')
-
+    
     # Walk through the directory and convert images
     for root, dirs, files in os.walk(directory):
         for file in files:
             if file.endswith(extensions):
                 image_path = os.path.join(root, file)
                 webp_path = image_path.rsplit('.', 1)[0] + '.webp'
-
+                
                 # Check if the .webp counterpart exists and if the image has been modified
                 if not check_webp_counterpart(image_path) or os.path.getmtime(image_path) > os.path.getmtime(webp_path):
                     if has_multiple_layers(image_path):
                         convert_image(image_path)
                     else:
                         convert_image(image_path)
-                    print(f"Converted {file} to WEBP.")
+                        print(f"Converted {file} to WEBP.")
 
 def main(image_dir):
-    # Traverse the directory tree recursively
+    # Process the 'assets/img' directory itself
+    process_directory(image_dir)
+    
+    # Traverse the directory tree recursively for subdirectories
     for root, dirs, files in os.walk(image_dir):
         for dir in dirs:
             process_directory(os.path.join(root, dir))
